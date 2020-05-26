aedcc22958c12365366bf6696ee2b043171596e4
Francesco Boscarino <francesco@bizmate.it>
2020-05-26 11:10:02 +0200

    Submit button to handle valid state

---
+++ lib/components/Submit.svelte
@@ -0,0 +1,16 @@
+<script>
+import { getContext } from "svelte";
+import { key } from "./key";
+
+const { state, updateTouched } = getContext(key);
+
+console.log($state)
+for(let field in $state.touched) {
+  console.log(field)
+}
+
+</script>
+
+<button {...$$props} type="submit" disabled={!$state.isValid}>
+  <slot />
+</button>
