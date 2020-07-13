0a28b9d961d5567c85febf4118f1625833b02e05
Larry Botha <larry@fixate.it>
2020-07-12 23:37:46 -0500

    test(jest config): add project select and typeahead watch plugins

--- test/config/jest.common.js
+++ test/config/jest.common.js
@@ -4,4 +4,10 @@ module.exports = {
   rootDir: path.resolve(__dirname, '../..'),
 
   roots: ['<rootDir>/lib', '<rootDir>/test', __dirname],
+
+  watchPlugins: [
+    'jest-watch-typeahead/filename',
+    'jest-watch-typeahead/testname',
+    'jest-watch-select-projects',
+  ],
 };
