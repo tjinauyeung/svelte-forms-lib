782cc3459dcc092868e8c924ab89cba54d744de1
Larry Botha <larry@fixate.it>
2021-02-03 23:52:22 +0200

    chore(tests): configure tests for testing svelte components

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,9 @@
 const jestCommon = require('./test/config/jest.common');
 const lintProject = require('./test/config/jest.lint');
-const unitTestProject = require('./test/config/jest.unit');
+const testProject = require('./test/config/jest.test');
 
 module.exports = {
   ...jestCommon,
 
-  projects: [unitTestProject, lintProject],
+  projects: [testProject, lintProject],
 };
