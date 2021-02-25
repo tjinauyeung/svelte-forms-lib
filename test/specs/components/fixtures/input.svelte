161ca006142b480ac565086fa4b34d8d01cf546e
Larry Botha <larry@fixate.it>
2021-02-26 01:22:30 +0200

    test(form component): test exposed props

---
+++ test/specs/components/fixtures/input.svelte
@@ -0,0 +1,14 @@
+<script>
+  import {getContext} from 'svelte';
+  import {key} from '../../../../lib';
+
+  export let field;
+
+  const context = getContext(key);
+  const form = context.form;
+</script>
+
+<label for={field}>
+  {field}
+  <input type="text" id={field} name={field} bind:value={$form[field]} />
+</label>
