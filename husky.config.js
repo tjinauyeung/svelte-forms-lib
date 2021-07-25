360872e1fd8b54a4cdf168a76716d74a9dd626f0
Larry Botha <larry@fixate.it>
2021-07-25 15:59:02 +0200

    chore(husky): update to husky 7 configs

--- husky.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  hooks: {
-    'pre-commit': 'npm run format',
-    'pre-push': 'npm test',
-    'commit-msg':
-      'commitlint -E HUSKY_GIT_PARAMS --help-url="https://github.com/tjinauyeung/svelte-forms-lib/#commit-message-conventions"',
-  },
-};
